b5b26e31468bd69c3c7f5a480c8d68c484b0292d
James Demetris <63110925+jbjd@users.noreply.github.com>
2022-11-30 21:17:17 -0500

    shuffle instead of random generator

--- main.cpp
+++ main.cpp
@@ -2,17 +2,15 @@
 #include <windows.h> 
 #include <gdiplus.h>
 #include <chrono>
-#include <thread>
-//#include <iostream>
-#include <random>
 #include <memory>
+//#include <iostream>
 
 const int TILESIZE = 30, 
-COL = 30,
-ROW = 16,
+COL = 30, ROW = 16,
 H = 480,
 TOP = 100,
-HEADERSIZE = 70;
+HEADERSIZE = 70,
+WID=COL*TILESIZE, HEI=ROW*TILESIZE;
 
 struct tile{
 	int nextTo = 0;
@@ -22,7 +20,8 @@ struct tile{
 };
 int bombs = 99, revealed = 0, // total mines and spaces revealed
 tick = 0, // this divided by 50 is time in seconds
-topLeftX, bottom;  // location of box on screen
+topLeftX, bottom,  // location of box on screen
+shuffledArray[480];  //shuffled and used for mine placement
 tile arr[H];
 bool inProgress = false, hasClicked = false;  // if timer should run and if user has clicked in current game
 
@@ -33,7 +32,7 @@ void countBombs(tile (&arr)[H]){
 	arr[H-COL].nextTo = arr[H-COL-COL].bomb+arr[H-COL+1].bomb+arr[H-COL-COL+1].bomb;  // sw corner
 	arr[H-1].nextTo = arr[H-2].bomb+arr[H-COL-1].bomb+arr[H-COL-2].bomb;  // se corner
 	// top row
-	int i;
+	int i, j, ind;
 	for(i = 1; i < COL-1; ++i){
 		arr[i].nextTo = arr[i-1].bomb+arr[i+1].bomb+arr[i+COL].bomb+arr[i+COL-1].bomb+arr[i+COL+1].bomb;
 	}
@@ -50,54 +49,43 @@ void countBombs(tile (&arr)[H]){
 		arr[i].nextTo = arr[i-COL].bomb+arr[i-COL-1].bomb+arr[i-1].bomb+arr[i+COL].bomb+arr[i+COL-1].bomb;
 	}
 	// all rows between top/bot
-	int ind;
-	for(int j = 1; j < ROW-1; ++j){
+	for(j = 1; j < ROW-1; ++j){
 		//for each tile in row
 		for(i = 1; i < COL-1; ++i){
 			ind = i+(j*COL);
 			arr[ind].nextTo = arr[ind-1].bomb+arr[ind+1].bomb+arr[ind-COL-1].bomb+arr[ind-COL].bomb+arr[ind-COL+1].bomb+arr[ind+COL-1].bomb+arr[ind+COL].bomb+arr[ind+COL+1].bomb;
 		}
 	}
-
-	/*for(int i = 0; i < ROW; ++i){
-		for(int j = 0; j < COL; ++j){
-			if(arr[j+(i*COL)].bomb)
-				std::cout << "* ";
-			else
-				std::cout << arr[j+(i*COL)].nextTo << ' ';
-		}
-		std::cout << '\n';
-	}
-	exit(0);*/
 }
 
 // Clocks for limiting fps
-std::chrono::time_point<std::chrono::steady_clock> nextF = std::chrono::steady_clock::now();
+using std::chrono::steady_clock;
+std::chrono::time_point<steady_clock> nextF = steady_clock::now();
 
 // global vars
 std::unique_ptr<Gdiplus::Graphics> graphics;  // gdiplus for drawing
 bool active = 1;  // if main loop continues
-RECT rect; // screen bbox
-Gdiplus::Color back = Gdiplus::Color(255, 192, 192, 192);
-Gdiplus::Color darkGray = Gdiplus::Color(255, 128, 128, 128);
-Gdiplus::Color backer = Gdiplus::Color(255, 184, 185, 184);
-Gdiplus::Color white = Gdiplus::Color(255, 240, 240, 240);
-Gdiplus::Color black = Gdiplus::Color(255, 1, 1, 1);
-Gdiplus::Color red = Gdiplus::Color(255, 230, 50, 10);
-Gdiplus::Color yel = Gdiplus::Color(255, 255, 255, 0);
-Gdiplus::Color one = Gdiplus::Color(255, 30, 50, 200);
-Gdiplus::Color two = Gdiplus::Color(255, 40, 110, 30);
-Gdiplus::Color three = Gdiplus::Color(255, 200, 50, 30);
-Gdiplus::Color four = Gdiplus::Color(255, 110, 40, 110);
-Gdiplus::Color five = Gdiplus::Color(255, 150, 30, 30);
-Gdiplus::Color six = Gdiplus::Color(255, 30, 110, 150);
-Gdiplus::Color sev = Gdiplus::Color(255, 200, 40, 160);
+tagRECT rect; // screen bbox
+Gdiplus::Color back = Gdiplus::Color(255, 192, 192, 192),
+darkGray = Gdiplus::Color(255, 128, 128, 128),
+backer = Gdiplus::Color(255, 184, 185, 184),
+white = Gdiplus::Color(255, 240, 240, 240),
+black = Gdiplus::Color(255, 1, 1, 1),
+red = Gdiplus::Color(255, 230, 50, 10),
+yel = Gdiplus::Color(255, 255, 255, 0),
+one = Gdiplus::Color(255, 30, 50, 200),
+two = Gdiplus::Color(255, 40, 110, 30),
+three = Gdiplus::Color(255, 200, 50, 30),
+four = Gdiplus::Color(255, 110, 40, 110),
+five = Gdiplus::Color(255, 150, 30, 30),
+six = Gdiplus::Color(255, 30, 110, 150),
+sev = Gdiplus::Color(255, 200, 40, 160);
 
 void getDimensions(const HWND &w){
 	GetWindowRect(w, &rect);
 	rect.right -= rect.left;
 	rect.bottom -= rect.top;
-	topLeftX = ((rect.right-(TILESIZE*30))/2)-1;
+	topLeftX = ((rect.right-(TILESIZE*30))>>1)-1;
 	bottom = 200+(TILESIZE*16);
 }
 
@@ -108,31 +96,28 @@ void updateTiles(){
 	Gdiplus::SolidBrush behind(black), oneMine(one), twoMine(two), threeMine(three), fourMine(four),
 	fiveMine(five), sixMine(six), sevMine(sev), eightMine(black), wiper(back), wiper2(backer);
 
-	Gdiplus::Bitmap bmp(TILESIZE*30, TILESIZE*16);
+	Gdiplus::Bitmap bmp(WID, HEI);
 	Gdiplus::Graphics* graph = Gdiplus::Graphics::FromImage(&bmp);
-	graph->FillRectangle(&wiper, 0, 0, TILESIZE*30, TILESIZE*16);
-	for(int j = 0; j < 16; ++j){
-		for(int i = 0; i < 30; ++i){
-			int x = (i*TILESIZE);
-			int y = (j*TILESIZE);
-			if(arr[(30*j)+i].hidden){
+	graph->FillRectangle(&wiper, 0, 0, WID, HEI);
+	int x, y, ind;
+	for(y = 0; y < HEI; y+=TILESIZE){
+		for(x = 0; x < WID; x+=TILESIZE){
+			ind = y+(x/TILESIZE);
+			if(arr[ind].hidden){
 				graph->DrawLine(&whitePen, x, y, x+TILESIZE-1, y);
 				graph->DrawLine(&whitePen, x, y, x, y+TILESIZE-1);
 				graph->DrawLine(&darkPen, x+TILESIZE-1, y, x+TILESIZE-1, y+TILESIZE);
 				graph->DrawLine(&darkPen, x, y+TILESIZE-1, x+TILESIZE-1, y+TILESIZE-1);
-				if(arr[(30*j)+i].flag){
+				if(arr[ind].flag){
 					graph->FillRectangle(&behind, x+7, y+22, (TILESIZE>>1)+1, 3);
 					graph->FillRectangle(&behind, x+14, y+9, 2, 13);
 					// red flag
-					Gdiplus::PointF p1(x+16, y+5);
-					Gdiplus::PointF p2(x+16, y+15);
-					Gdiplus::PointF p3(x+8, y+10);
-					Gdiplus::PointF points[3] ={p1,p2,p3};
+					Gdiplus::PointF p1(x+16, y+5), p2(x+16, y+15), p3(x+8, y+10),
+					points[3] = {p1,p2,p3};
 					graph->FillPolygon(&threeMine, points, 3);
 				}
-			}else if(arr[(30*j)+i].bomb){
-				Gdiplus::SolidBrush base(black);
-				Gdiplus::SolidBrush back(red);
+			}else if(arr[ind].bomb){
+				Gdiplus::SolidBrush base(black), back(red);
 				Gdiplus::Pen lines(black, 2);
 
 				graph->FillRectangle(&back, x, y, TILESIZE-1, TILESIZE-1);
@@ -141,34 +126,34 @@ void updateTiles(){
 				graph->DrawLine(&lines, x+5, y+15, x+25, y+15);
 			}else{
 				graph->FillRectangle(&wiper2, x, y, TILESIZE-1, TILESIZE-1);
-				if(arr[(30*j)+i].nextTo){
-					Gdiplus::PointF pointF(x+7, y+5);
-					switch(arr[(30*j)+i].nextTo){
-						case 1:
-							graph->DrawString(L"1", 1, &font, pointF, NULL, &oneMine);
-							break;
-						case 2:
-							graph->DrawString(L"2", 1, &font, pointF, NULL, &twoMine);
-							break;
-						case 3:
-							graph->DrawString(L"3", 1, &font, pointF, NULL, &threeMine);
-							break;
-						case 4:
-							graph->DrawString(L"4", 1, &font, pointF, NULL, &fourMine);
-							break;
-						case 5:
-							graph->DrawString(L"5", 1, &font, pointF, NULL, &fiveMine);
-							break;
-						case 6:
-							graph->DrawString(L"6", 1, &font, pointF, NULL, &sixMine);
-							break;
-						case 7:
-							graph->DrawString(L"7", 1, &font, pointF, NULL, &sevMine);
-							break;
-						default:
-							graph->DrawString(L"0", 1, &font, pointF, NULL, &eightMine);
-							break;
-					}
+				Gdiplus::PointF pointF(x+7, y+5);
+				switch(arr[ind].nextTo){
+					case 1:
+						graph->DrawString(L"1", 1, &font, pointF, &oneMine);
+						break;
+					case 2:
+						graph->DrawString(L"2", 1, &font, pointF, &twoMine);
+						break;
+					case 3:
+						graph->DrawString(L"3", 1, &font, pointF, &threeMine);
+						break;
+					case 4:
+						graph->DrawString(L"4", 1, &font, pointF, &fourMine);
+						break;
+					case 5:
+						graph->DrawString(L"5", 1, &font, pointF, &fiveMine);
+						break;
+					case 6:
+						graph->DrawString(L"6", 1, &font, pointF, &sixMine);
+						break;
+					case 7:
+						graph->DrawString(L"7", 1, &font, pointF, &sevMine);
+						break;
+					case 8:
+						graph->DrawString(L"8", 1, &font, pointF, &eightMine);
+						break;
+					default:
+						break;
 				}
 			}
 		}
@@ -181,12 +166,13 @@ void updateTiles(){
 // redraws hidden tile, dont call for revealed tiles
 void redrawTile(const int &x, const int &y){
 	Gdiplus::SolidBrush wiper(back), base(black);
-	int xc = topLeftX+2+(x*TILESIZE);
-	int yc = TOP+HEADERSIZE+14+(y*TILESIZE);
+	int xc = topLeftX+2+(x*TILESIZE),
+	yc = TOP+HEADERSIZE+14+(y*TILESIZE),
+	ind = (30*y)+x;
 	// draw gray box and put special stuff on top
 	graphics->FillRectangle(&wiper, xc+1, yc+1, TILESIZE-2, TILESIZE-2);
 
-	if(arr[(30*y)+x].bomb && !arr[(30*y)+x].hidden){
+	if(arr[ind].bomb && !arr[ind].hidden){
 		Gdiplus::SolidBrush back(red);
 		Gdiplus::Pen lines(black, 2);
 
@@ -195,51 +181,50 @@ void redrawTile(const int &x, const int &y){
 		graphics->DrawLine(&lines, xc+15, yc+5, xc+15, yc+25);
 		graphics->DrawLine(&lines, xc+5, yc+15, xc+25, yc+15);
 	}
-	else if(arr[(30*y)+x].flag){
+	else if(arr[ind].flag){
 		Gdiplus::SolidBrush flag(three);
 		graphics->FillRectangle(&base, xc+7, yc+22, (TILESIZE>>1)+1, 3);
 		graphics->FillRectangle(&base, xc+14, yc+9, 2, 13);
 		// red flag
-		Gdiplus::PointF p1(xc+16, yc+5);
-		Gdiplus::PointF p2(xc+16, yc+15);
-		Gdiplus::PointF p3(xc+8, yc+10);
-		Gdiplus::PointF points[3] ={p1,p2,p3};
+		Gdiplus::PointF p1(xc+16, yc+5), p2(xc+16, yc+15), p3(xc+8, yc+10),
+		points[3] ={p1,p2,p3};
 		graphics->FillPolygon(&flag, points, 3);
 	}
 }
 
-void addMines(tile (&arr)[H]){
-	std::minstd_rand  rng(time(NULL));
-	std::uniform_int_distribution<std::minstd_rand0::result_type> dist(0, H-1);
-	int total = 0;
-	int temp;
-	while(total < 99){
-		temp = dist(rng);
-		if(!arr[temp].bomb){
-			arr[temp].bomb = true;
-			++total;
-		}
+void shuffle(int (&shuffledArray)[H]){
+	srand(time(0));
+	int i, ind, temp;
+	for (i = 0; i < H; ++i){
+		ind = rand() % H;
+		temp = shuffledArray[ind];
+		shuffledArray[ind] = shuffledArray[i];
+		shuffledArray[i] = temp;
+	}
+}
+
+void addMines(tile (&arr)[H], int (&shuffledArray)[H]){
+	shuffle(shuffledArray);
+	for(int i = 0; i < 99; ++i){
+		arr[shuffledArray[i]].bomb = true;
 	}
 }
 
 // adds mines and gives each tile count of nearby mines
 void setup(tile (&arr)[H]){
-	addMines(arr);
+	addMines(arr, shuffledArray);
 	countBombs(arr);
 }
 
 void resetTiles(tile (&arr)[H]){
 	bombs = 99;
-	revealed = 0;
+	revealed = tick = hasClicked = 0;
 	for(int i = 0; i < H; ++i){
 		arr[i].hidden = true;
-		arr[i].bomb = false;
+		arr[i].bomb = arr[i].flag = false;
 		arr[i].nextTo = 0;
-		arr[i].flag = false;
 	}
-	tick = 0;
 	inProgress = true;
-	hasClicked = false;
 	setup(arr);
 	updateTiles();
 }
@@ -251,10 +236,9 @@ void drawState(){
 	Gdiplus::SolidBrush wiper(back);
 	graph->FillRectangle(&wiper, 0, 0, rect.right, rect.bottom);
 	// header
-	Gdiplus::Pen darkPen(darkGray);
+	Gdiplus::Pen darkPen(darkGray), whitePen(white);
 	graph->DrawLine(&darkPen, topLeftX, TOP, topLeftX, TOP+HEADERSIZE);
 	graph->DrawLine(&darkPen, topLeftX, TOP, rect.right-topLeftX, TOP);
-	Gdiplus::Pen whitePen(white);
 	graph->DrawLine(&whitePen, rect.right-topLeftX, TOP, rect.right-topLeftX, TOP+HEADERSIZE);
 	graph->DrawLine(&whitePen, topLeftX, TOP+HEADERSIZE, rect.right-topLeftX, TOP+HEADERSIZE);
 	// draw main area
@@ -277,18 +261,14 @@ void updateHeader(){
 	Gdiplus::PointF pointF(0, 1);
 	
 	// timer
-	int time = tick/50;
-	int a;
-	WCHAR timer[4] = L"000";
+	unsigned time = tick/50, temp;
+	wchar_t timer[4];
 	if(time < 10){
-		timer[2] += time;
+		swprintf(timer, L"00%d\0", time);
 	}else if(time < 100){
-		timer[1] += time/10;
-		timer[2] += time%10;
+		swprintf(timer, L"0%d\0", time);
 	}else{
-		timer[0] += time/100;
-		timer[1] += (time%100)/10;
-		timer[2] += time%10;
+		swprintf(timer, L"%d\0", time);
 	}
 	Gdiplus::Bitmap bmp(66, 40);
 	Gdiplus::Graphics* graph = Gdiplus::Graphics::FromImage(&bmp);
@@ -298,24 +278,17 @@ void updateHeader(){
 	graphics->DrawImage(&bmp, rect.right-topLeftX-86, TOP+15, 66, 40);
 	delete graph;
 
-	WCHAR bombCount[4] = L"000";
+	wchar_t bombCount[4];
 	if(bombs > 9){
-		bombCount[1] += bombs/10;
-		bombCount[2] += bombs%10;
+		swprintf(bombCount, L"0%d\0", bombs);
 	}else if(bombs >= 0){
-		bombCount[2] += bombs;
+		swprintf(bombCount, L"00%d\0", bombs);
 	}else if(bombs >= -9){
-		bombCount[0] = L'-';
-		bombCount[2] += -1*bombs;
+		swprintf(bombCount, L"-0%d\0", -1*bombs);
 	}else if(bombs >= -99){
-		bombCount[0] = L'-';
-		a = -1*bombs;
-		bombCount[1] += a/10;
-		bombCount[2] += a%10;
+		swprintf(bombCount, L"%d\0", bombs);
 	}else{
-		bombCount[0] = L'-';
-		bombCount[1] = L'-';
-		bombCount[2] = L'-';
+		swprintf(bombCount, L"---\0");
 	}
 	graph = Gdiplus::Graphics::FromImage(&bmp);
 	graph->FillRectangle(&behind, 0, 0, 66, 40);
@@ -337,75 +310,42 @@ void updateHeader(){
 	//mouth
 	graph->DrawArc(&bPen, 15, 20, 10, 5, 180, -180);
 	
-	graphics->DrawImage(&face, ((int)(rect.right/2))-20, TOP+15, 40, 40);
+	graphics->DrawImage(&face, (rect.right>>1)-20, TOP+15, 40, 40);
 	delete graph;
 }
 
-void showNear(int x, int y){
+void showNear(const int &x, const int &y);
+void checkZero(const int &x, const int &y, const int &ind){
+	if(arr[ind].hidden){
+		arr[ind].hidden = false;
+		++revealed;
+		if(arr[ind].nextTo == 0){
+			showNear(x, y);
+		}
+	}
+}
+
+void showNear(const int &x, const int &y){
 	if(y > 0){
-		if(arr[(30*(y-1))+x].hidden){
-			arr[(30*(y-1))+x].hidden = false;
-			++revealed;
-			if(!arr[(30*(y-1))+x].nextTo){
-				showNear(x, y-1);
-			}
-		}
-		if(x > 0 && arr[(30*(y-1))+x-1].hidden){
-			arr[(30*(y-1))+x-1].hidden = false;
-			++revealed;
-			if(!arr[(30*(y-1))+x-1].nextTo){
-				showNear(x-1, y-1);
-			}
-		}
-		if(x < 29 && arr[(30*(y-1))+x+1].hidden){
-			arr[(30*(y-1))+x+1].hidden = false;
-			++revealed;
-			if(!arr[(30*(y-1))+x+1].nextTo){
-				showNear(x+1, y-1);
-			}
-		}
+		checkZero(x, y-1, (30*(y-1))+x);
+		if(x > 0) checkZero(x-1, y-1, (30*(y-1))+x-1);
+		if(x < 29) checkZero(x+1, y-1, (30*(y-1))+x+1);
 	}
 	if(y < 15){
-		if(arr[(30*(y+1))+x].hidden){
-			arr[(30*(y+1))+x].hidden = false;
-			++revealed;
-			if(!arr[(30*(y+1))+x].nextTo){
-				showNear(x, y+1);
-			}
-		}
-		if(x > 0 && arr[(30*(y+1))+x-1].hidden){
-			arr[(30*(y+1))+x-1].hidden = false;
-			++revealed;
-			if(!arr[(30*(y+1))+x-1].nextTo){
-				showNear(x-1, y+1);
-			}
-		}
-		if(x < 29 && arr[(30*(y+1))+x+1].hidden){
-			arr[(30*(y+1))+x+1].hidden = false;
-			++revealed;
-			if(!arr[(30*(y+1))+x+1].nextTo){
-				showNear(x+1, y+1);
-			}
-		}
+		checkZero(x, y+1, (30*(y+1))+x);
+		if(x > 0) checkZero(x-1, y+1, (30*(y+1))+x-1);
+		if(x < 29) checkZero(x+1, y+1, (30*(y+1))+x+1);
 	}
-	if(x > 0 && arr[(30*(y))+x-1].hidden){
-		arr[(30*(y))+x-1].hidden = false;
-		++revealed;
-		if(!arr[(30*(y))+x-1].nextTo){
-			showNear(x-1, y);
-		}
+	if(x > 0){
+		checkZero(x-1, y, (30*(y))+x-1);
 	}
-	if(x < 29 && arr[(30*(y))+x+1].hidden){
-		arr[(30*(y))+x+1].hidden = false;
-		++revealed;
-		if(!arr[(30*(y))+x+1].nextTo){
-			showNear(x+1, y);
-		}
+	if(x < 29){
+		checkZero(x+1, y, (30*(y))+x+1);
 	}
 }
 
 //handles events sent to window
-LRESULT CALLBACK windowProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam) {
+long long __stdcall windowProc(_In_ HWND__* hwnd, _In_ unsigned uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam) {
 	LPMINMAXINFO lpMMI = (LPMINMAXINFO)lParam;
 	int x, y;
 	switch(uMsg){
@@ -423,8 +363,6 @@ LRESULT CALLBACK windowProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, 
 		case WM_DESTROY:
 			active = 0;
 			return 0;
-		case WM_PAINT:
-			return 0;
 		case WM_GETMINMAXINFO:
 			lpMMI->ptMinTrackSize.x = 1000;
 			lpMMI->ptMinTrackSize.y = 800;
@@ -433,8 +371,7 @@ LRESULT CALLBACK windowProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, 
 			x = (lParam & 0xFFFF);
 			y = (lParam >> 16);
 			// check if user clicked on new game
-			// ((int)(rect.right/2))-20, TOP+15, 40, 40
-			if(x >= ((int)(rect.right/2))-20 && x <= ((int)(rect.right/2))+20 && y >= TOP+15 && y <= TOP+55){
+			if(x >= (rect.right>>1)-20 && x <= (rect.right>>1)+19 && y >= TOP+15 && y <= TOP+55){
 				// must have clicked on face
 				resetTiles(arr);
 			}
@@ -507,18 +444,18 @@ LRESULT CALLBACK windowProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, 
 }
 
 //main function
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine ,int nShowCmd){
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char* lpCmdLine, int nShowCmd){
 	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
-	ULONG_PTR gdiplusToken;
-	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-	WNDCLASS windowClass = {};
+	unsigned long long gdiplusToken;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, 0);
+	tagWNDCLASSA windowClass = {};
 	windowClass.style = CS_HREDRAW | CS_VREDRAW;
 	windowClass.lpszClassName = "Game Window";
 	windowClass.lpfnWndProc = windowProc;
-	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	RegisterClass(&windowClass);
-	HWND window = CreateWindowA(windowClass.lpszClassName, "MineSweeper",  WS_VISIBLE | WS_MAXIMIZE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hInstance, 0);
-	HDC hdc = GetDC(window);
+	windowClass.hCursor = LoadCursor(0, IDC_ARROW);
+	RegisterClassA(&windowClass);
+	HWND__ *window = CreateWindowA(windowClass.lpszClassName, "MineSweeper",  WS_VISIBLE | WS_MAXIMIZE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hInstance, 0);
+	HDC__ *hdc = GetDC(window);
 	ShowWindow(window, SW_MAXIMIZE);
 
 	tagMSG message; //gets messages sent to window and reads them
@@ -537,6 +474,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine ,int n
 
 	for (int i = 0; i < H; ++i) {
 		arr[i] = tile();
+		shuffledArray[i] = i;
 	}
 
 	setup(arr);
@@ -545,7 +483,10 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine ,int n
 	while (active) {
 		// FPS Limiter
 		nextF += std::chrono::milliseconds(20); 
-		std::this_thread::sleep_for(nextF - std::chrono::steady_clock::now());
+		long long sleepFor = std::chrono::duration_cast<std::chrono::milliseconds>(nextF - steady_clock::now()).count();
+		if(sleepFor > 0){
+			Sleep(sleepFor);
+		}
 		if(inProgress&&hasClicked&&tick<(999*50)) ++tick;
 		if(PeekMessage(&message, window, 0, 0, PM_REMOVE | PM_NOYIELD)){//this while loop processes message for window
 			TranslateMessage(&message); 
